f624152c5a10c8af80c598df7ad826d1151d2746
Neha Gupta <neha45556@gmail.com>
2019-03-03 00:29:28 -0800

    unit-testandCmake

--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -16,11 +16,10 @@ using namespace std;
 TEST(SingleCommand, CommandEcho) {
     char inputArray [] = "echo hello";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* command = new FullCommand();
+    command->parse(0, 0, input);
     testing::internal::CaptureStdout();
-    execute->execute();
+    command->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = "hello\n";
     EXPECT_EQ(expectedOutput, output);
@@ -29,11 +28,10 @@ TEST(SingleCommand, CommandEcho) {
 TEST(SingleCommand, CommandLS) {
     char inputArray [] = "ls -a";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* command = new FullCommand();
+    command->parse(0,0,input);
     testing::internal::CaptureStdout();
-    execute->execute();
+    command->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = ".\n..\na.out\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\n.git\n.gitignore\n.gitmodules\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
     EXPECT_EQ(expectedOutput, output);
@@ -43,31 +41,28 @@ TEST(SingleCommand, CommandLS) {
 TEST(SingleCommand, MKDirAndRM) {
     char inputArray [] = "mkdir hello";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
-    execute->execute();
+    FullCommand* command = new FullCommand();
+    command->parse(0, 0, input);
+    command->execute();
     
     char input2Array [] = "ls -a";
     Input* inputCheck = new Input(input2Array);
-    Execute* executeCheck = new Execute();
-    executeCheck->populateExecute(0, 0, inputCheck);
-    executeCheck->populateExecuteConnectors(inputCheck);
+    FullCommand* command2 = new FullCommand();
+    command2->parse(0, 0, inputCheck);
     testing::internal::CaptureStdout();
-    executeCheck->execute();
+    command2->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = ".\n..\na.out\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\n.git\n.gitignore\n.gitmodules\ngoogletest\nhello\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
     EXPECT_EQ(expectedOutput, output);
     
     char input3Array [] = "rm -rf hello";
     Input* input3 = new Input(input3Array);
-    Execute* execute2 = new Execute();
-    execute2->populateExecute(0, 0, input3);
-    execute2->populateExecuteConnectors(input3);
-    execute2->execute();
+    FullCommand* command3  = new FullCommand();
+    command3->parse(0, 0, input3);
+    command3->execute();
     
     testing::internal::CaptureStdout();
-    executeCheck->execute();
+    command2->execute();
     string output2 = testing::internal::GetCapturedStdout();
     string expectedOutput2 = ".\n..\na.out\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\n.git\n.gitignore\n.gitmodules\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\n";
     EXPECT_EQ(expectedOutput2, output2); 
@@ -76,11 +71,10 @@ TEST(SingleCommand, MKDirAndRM) {
 TEST(SingleCommand, WrongCommand) {
     char inputArray [] = "wrong hello";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* command = new FullCommand();
+    command->parse(0, 0, input);
     testing::internal::CaptureStdout();
-    execute->execute();
+    command->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = "";
     EXPECT_EQ("", output);
@@ -89,11 +83,10 @@ TEST(SingleCommand, WrongCommand) {
 TEST(MultipleCommand, ManyCommands) {
     char inputArray [] = "ls; echo hello && echo 1 || echo 2; echo 3";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* command = new FullCommand();
+    command->parse(0, 0, input);
     testing::internal::CaptureStdout();
-    execute->execute();
+    command->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = "a.out\nbin\nCMakeCache.txt\nCMakeFiles\ncmake_install.cmake\nCMakeLists.txt\ngoogletest\nimages\nintegration_tests\nlib\nMakefile\nREADME.md\nrshell\nsrc\ntest\nunit_tests\nhello\n1\n3\n";
     EXPECT_EQ(expectedOutput, output);
@@ -102,11 +95,10 @@ TEST(MultipleCommand, ManyCommands) {
 TEST(SingleCommand, Quotes) {
     char inputArray [] = "echo \"hello && world\"";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* command = new FullCommand();
+    command->parse(0, 0, input);
     testing::internal::CaptureStdout();
-    execute->execute();
+    command->execute();
     string output = testing::internal::GetCapturedStdout();
     string expectedOutput = "hello && world\n";
     EXPECT_EQ(expectedOutput, output);
@@ -114,9 +106,8 @@ TEST(SingleCommand, Quotes) {
 TEST(MultipleCommand, QuotesWithConnectors) {
     char inputArray [] = "echo \"hello && world\"; echo \"Basket && ball\"";
     Input* input = new Input(inputArray);
-    Execute* execute = new Execute();
-    execute->populateExecute(0, 0, input);
-    execute->populateExecuteConnectors(input);
+    FullCommand* execute = new FullCommand();
+    execute->parse(0, 0, input);
     testing::internal::CaptureStdout();
     execute->execute();
     string output = testing::internal::GetCapturedStdout();
@@ -126,9 +117,8 @@ TEST(MultipleCommand, QuotesWithConnectors) {
 TEST(SingleCommand, Comment) {
 	char inputArray [] = "echo hello # world";
 	Input* input = new Input(inputArray);
-	Execute* execute = new Execute();
-	execute->populateExecute(0, 0, input);
-	execute->populateExecuteConnectors(input);
+	FullCommand* execute = new FullCommand();
+	execute->parse(0, 0, input);
 	testing::internal::CaptureStdout();
 	execute->execute();
 	string output = testing::internal::GetCapturedStdout();
@@ -138,11 +128,10 @@ TEST(SingleCommand, Comment) {
 TEST(SingleCommand, CommentWithQuotes) {
 	char inputArray [] = "echo \"hello # world\"";
 	Input* input = new Input(inputArray);
-	Execute* execute = new Execute();
-	execute->populateExecute(0, 0, input);
-	execute->populateExecuteConnectors(input);
+	FullCommand* command = new FullCommand();
+	command->parse(0, 0, input);
 	testing::internal::CaptureStdout();
-	execute->execute();
+	command->execute();
 	string output = testing::internal::GetCapturedStdout();
 	string expectedOutput = "hello \n";
 	EXPECT_EQ(expectedOutput, output);
