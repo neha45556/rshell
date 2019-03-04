b1c6a3171345f2f0a722161e369007bb94bea2e6
Neha Gupta <neha45556@gmail.com>
2019-03-03 18:31:27 -0800

    fixed tests

--- integration_tests/test_command_tests.sh
+++ integration_tests/test_command_tests.sh
@@ -1,6 +1,5 @@
-#test_command_tests.sh
 
-#!/bin/s
+#!/bin/sh
 cd ..
 
 ./rshell << EOF
@@ -9,6 +8,10 @@ test -f src/input.cpp
 test -d ../src && echo “exists”
 test -e src/stuff.cpp || echo “failed”
 test -d direc 
+test -e src/connector.h && test -e src/command.cpp && test -d ../src && echo "files"
+echo testing && echo "files" && test -e src/MultCmd.cpp && echo working
+test -d ../integration_test && echo hi || echo bye
+test unit_tests/unit_tests.cpp && (echo hi && echo hello) || echo "world"
 test src/command.h
 (test test.cpp)
 exit
