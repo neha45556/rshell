e76f5c68059abc624030b269b177d68cb4734b01
Marc Jimenez <marcjimenez999@gmail.com>
2019-03-03 19:34:28 -0800

    Added unit tests

--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -305,14 +305,14 @@ TEST(SingleCommand, ParenQuotes) {
         EXPECT_EQ(expectedOutput, output);
 }
 TEST(SingleCommand, ParenQuotesCon) {
-	char inputArray [] = "(echo \"1; 2\"; echo \"3 && 4\"; echo \"5 || 6\")";
+	char inputArray [] = "(echo \"1; 2\"; echo \"3 ||4\"";
         Input* input = new Input(inputArray);
         FullCommand* command = new FullCommand();
         command->parse(0, 0, input);
         testing::internal::CaptureStdout();
         command->execute();
         string output = testing::internal::GetCapturedStdout();
-        string expectedOutput = "1;2\n3 && 4\n5 || 6\n";
+        string expectedOutput = "1;2\n3 ||4\n";
         EXPECT_EQ(expectedOutput, output);
 }
 
