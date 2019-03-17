3b1c8c4811094c8a39dd03ad94e7289935d0abac
neha45556 <neha45556@gmail.com>
2019-03-16 23:48:06 -0700

    output

---
+++ integration_tests/output_redirection_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ..
+
+./rshell << EOF
+echo abc > integration_tests/outputfile.txt
+echo "output" && cat < integration_tests/outputfile.txt
+echo b >> integration_tests/outputfile.txt
+echo "in" && cat < integration_tests/outputfile.txt
+echo abcde >> integration_tests/outputfile.txt && echo efgh >> integration_tests/outputfile.txt
+echo "hello" < integration_tests/outputfile.txt && echo bye
+echo xyz < integration_tests/outputfile.txt || echo werq < integration_tests/outputfile.txt
+exit
+EOF
