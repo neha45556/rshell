bd366e90985bbc52c89e5e9c16e90b27ca23242f
Neha Gupta <neha45556@gmail.com>
2019-03-17 01:00:28 -0700

    files

--- integration_tests/input_redirection_tests.sh
+++ integration_tests/input_redirection_tests.sh
@@ -5,11 +5,11 @@ cd ..
 ./rshell << EOF
 cat < integration_tests/inputfile.txt
 echo hello; cat < integration_tests/inputfile.txt
-cat < intergration_tests/inputfile.txt; echo bye
-cat < intergration_tests/inputfile.txt && echo hi
-echo hi && cat < intergration_tests/outputfile.txt
-cat < intergration_tests/inputfile.txt > intergration_tests/outputfile.txt
-cat < intergration_tests/inputfile.txt || echo hi
+cat < integration_tests/inputfile.txt; echo bye
+cat < integration_tests/inputfile.txt && echo hi
+echo hi && cat < integration_tests/outputfile.txt
+cat < integration_tests/inputfile.txt > integration_tests/outputfile.txt
+cat < integration_tests/inputfile.txt || echo hi
 exit 
 EOF
 
--- integration_tests/pipe_operator_tests.sh
+++ integration_tests/pipe_operator_tests.sh
@@ -3,14 +3,14 @@
 cd ..
 
 ./rshell << EOF
-cat < intergration_tests/inputfile.txt | tr a-z A-Z
+cat < integration_tests/inputfile.txt | tr a-z A-Z
 echo HER | tr A-Z a-z | tr a-z A-Z
-cat < intergration_tests/inputfile.txt | tee intergration_tests/outputfile.txt | tr a-z A-Z
+cat < integration_tests/inputfile.txt | tee integration_tests/outputfile.txt | tr a-z A-Z
 echo ABC | tr A-Z a-z 
-cat < intergration_tests/inputfile.txt | tr A-Z a-z | tr a-z A-Z
-cat < intergration_tests/inputfile.txt | sort
-cat intergration_tests/inputfile.txt | tr a-z A-Z | sort > intergration_tests/outputfile.txt
-cat intergration_tests/inputfile.txt | tr a-z A-Z | tee intergration_tests/inputfile.txt | tr a-z A-Z >> intergration_tests/inputfile.txt
+cat < integration_tests/inputfile.txt | tr A-Z a-z | tr a-z A-Z
+cat < integration_tests/inputfile.txt | sort
+cat < integration_tests/inputfile.txt | tr a-z A-Z | sort > integration_tests/outputfile.txt
+cat < integration_tests/inputfile2.txt | tr A-Z a-z | tee integration_tests/outputfile.txt | tr a-z A-Z >> integration_tests/outputfile.txt
 exit 
 EOF
 
