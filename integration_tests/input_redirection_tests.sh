a94a4c822147d755ab860f138a6b1fd73ea889d0
neha45556 <neha45556@gmail.com>
2019-03-16 23:09:39 -0700

    Update input_redirection_tests.sh

--- integration_tests/input_redirection_tests.sh
+++ integration_tests/input_redirection_tests.sh
@@ -3,8 +3,12 @@
 cd ..
 
 ./rshell << EOF
-cat < intergration_tests/inputfile.txt
-echo hello; cat < integration_tests/inputfile.txt
+cat < intergration_tests/inputfile
+echo hello; cat < integration_tests/inputfile
+cat < inputfile | tr a-z A-Z
+cat < inputfile | tee outputfile | tr a-z A-Z
+echo hi && cat < outputfile
+
 exit 
 EOF
 
